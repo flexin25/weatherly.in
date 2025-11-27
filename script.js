df1363b3ae8cf9ecdab55108c41f43877084b67f
Abhishek <105061451+flexin25@users.noreply.github.com>
2025-11-27 14:20:03 +0530

    Initial commit: Weatherly weather app with landing page, about, info, and weather widget

---
+++ script.js
@@ -0,0 +1,136 @@
+const cityInput = document.querySelector('.city-input');
+const searchButton = document.querySelector('.search-btn')
+
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+const weatherInfoSection = document.querySelector('.weather-info');
+
+const countryTxt = document.querySelector('.country-txt');
+const temperatureTxt = document.querySelector('.temp-text');
+const descriptionTxt = document.querySelector('.condition-txt');
+const humidityTxt = document.querySelector('.humidity-value-txt');
+const pressureTxt = document.querySelector('.pressure-value-txt');
+const windValueTxt = document.querySelector('.wind-value-txt');
+const weatherSummaryImg = document.querySelector('.weather-summary-img');
+const currentDateTxt = document.querySelector('.current-date-txt');
+
+const forecastItemsContainer = document.querySelector('.forecast-items-container');
+
+const apiKey = '04c90abb0c88d8da11534c112a244bc9';
+
+searchButton.addEventListener('click', () => {
+    if (cityInput.value.trim() === '') return;
+    updateWeatherInfo(cityInput.value)
+    cityInput.value = '';
+    cityInput.blur();
+});
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(apiUrl)
+    return response.json();
+}
+
+function getWeatherIcon(id) {
+    if (id <= 232) return 'thunderstorm.svg';
+    if (id <= 321) return 'drizzle.svg';
+    if (id <= 531) return 'rain.svg';
+    if (id <= 622) return 'snow.svg';
+    if (id <= 781) return 'atmosphere.svg';
+    if (id <= 800) return 'clear.svg';
+    else return 'clouds.svg';
+}
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short'
+    };
+    return currentDate.toLocaleDateString('en-GB', options);
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city);
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection);
+        return;
+    }
+    console.log(weatherData);
+
+    const {
+        name: country,
+        main: { temp, humidity, pressure },
+        weather: [{ id, main }],
+        wind: { speed },
+    } = weatherData;
+
+    countryTxt.textContent = country;
+    temperatureTxt.textContent = `${Math.round(temp)}°C`;
+    descriptionTxt.textContent = main;
+    humidityTxt.textContent = `${humidity}%`;
+    pressureTxt.textContent = `${pressure} hPa`;
+    windValueTxt.textContent = `${speed.toFixed(1)} M/s`;
+    weatherSummaryImg.src = `assets/weather/${getWeatherIcon(id)}`;
+
+    await updateForecastInfo(city);
+
+    currentDateTxt.textContent = getCurrentDate();
+
+    showDisplaySection(weatherInfoSection);
+}
+
+async function updateForecastInfo(city) {
+    const forecastsData = await getFetchData('forecast', city);
+    if (!forecastsData || !forecastsData.list) return;
+
+    const timeTaken = '12:00:00';
+    const todayDate = new Date().toISOString().split('T')[0];
+
+    forecastItemsContainer.innerHTML = '';
+
+    const filtered = forecastsData.list.filter(forecast => {
+        return forecast.dt_txt.includes(timeTaken) && !forecast.dt_txt.includes(todayDate);
+    });
+
+    filtered.forEach(forecast => {
+        updateForecastItems(forecast);
+    });
+}
+
+function updateForecastItems(weatherData) {
+    const {
+        dt_txt: date,
+        weather: [{ id }],
+        main: { temp },
+    } = weatherData;
+
+    const formattedDate = formatDate(date);
+    const forecastItem = `
+        <div class="forecast-item">
+            <h5 class="forecast-item-date regular-txt">${formattedDate}</h5>
+            <img src="assets/weather/${getWeatherIcon(id)}" class="forecast-item-img" alt="">
+            <h5 class="forecast-item-temp">${Math.round(temp)}°C</h5>
+        </div>`;
+
+    forecastItemsContainer.insertAdjacentHTML('beforeend', forecastItem);
+}
+
+function formatDate(dateStr) {
+    const d = new Date(dateStr);
+    return d.toLocaleDateString('en-GB', { day: '2-digit', month: 'short' });
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, notFoundSection, searchCitySection].forEach(section => section.style.display = 'none');
+    section.style.display = 'flex';
+}
+
