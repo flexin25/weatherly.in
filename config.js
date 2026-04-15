36ea4037688f84dd81631797ea5dc1bbedbb7ba6
bristi <bardhanabhishek50@gmail.com>
2026-04-16 04:36:59 +0530

    Update site content and styling

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    OPENWEATHER_API_KEY: '04c90abb0c88d8da11534c112a244bc9'
+    OPENWEATHER_API_KEY: "your_actual_api_key"
 };
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = config;
